1ac9a340f3201d22ba8d0800f0f436fae13235b4
AgustiGM <53912168+AgustiGM@users.noreply.github.com>
2022-10-04 10:37:06 +0200

    Added some logic to clean data

--- data-extractor.py
+++ data-extractor.py
@@ -2,12 +2,13 @@ from datetime import datetime
 
 import dateutil
 import pandas as pd
+from dateutil.parser import ParserError
 
 
 def converter(date_time):
     try:
         return dateutil.parser.parse(date_time)
-    except:
+    except Exception:
         return dateutil.parser.parse('01/01/1900 12:00:00 AM')
 
 
@@ -22,3 +23,6 @@ new_df = df[df.Dataf > today]
 
 with open('test.csv', 'w', encoding='utf-8') as file:
     new_df.to_csv(file, index=False)
+
+with open('test.json', 'w', encoding='utf-8') as file:
+    new_df.to_json(file,orient='split', indent=4, index=False)
---
+++ data-normalization.py
@@ -0,0 +1,46 @@
+import json
+from datetime import datetime
+
+import dateutil
+
+
+def converter(date_time):
+    if type(date_time) is int:
+        try:
+            return datetime.utcfromtimestamp(date_time / 1000)
+        except:
+            return dateutil.parser.parse('01/01/1900 12:00:00 AM')
+    else:
+        try:
+            return dateutil.parser.parse(date_time)
+        except Exception:
+            return dateutil.parser.parse('01/01/1900 12:00:00 AM')
+
+
+file = open('test.json', 'r', encoding='utf-8')
+data = json.load(file)
+
+headers = data['columns']
+clean_data = []
+for j, record in enumerate(data['data']):
+    temp_record = {}
+    for i, column in enumerate(headers):
+        if "Data" in column or "Dataf" in column:
+            temp_record[column] = str(converter(record[i]))
+
+        elif 'Tags' in column:
+            if record[i] is not None:
+                tags = record[i].split(',')
+                new_tags = []
+                for tag in tags:
+                    new_tags.append(tag.split('/')[1])
+                temp_record[column] = ','.join(new_tags)
+            else:
+                temp_record[column] = None
+        else:
+            temp_record[column] = record[i]
+    clean_data.append(temp_record)
+
+with open('cleaned_data.json', 'w', encoding='utf-8') as file:
+    info = json.dumps(clean_data, indent=4)
+    print(info,file=file)
