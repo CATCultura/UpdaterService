c3f84ef99ce5cff307afce4592c0484777f0f00e
agusti <agustigm@gmail.com>
2022-12-12 12:07:30 +0100

    further checks

--- extract_new_data.py
+++ extract_new_data.py
@@ -5,6 +5,7 @@ from dateutil import utils
 import requests
 
 from service.DataCleaner import DataCleaner
+from service.DataEnricher import DataEnricher
 
 today = dateutil.utils.today()
 str_today = today.strftime('%Y-%m-%dT%H:%M:%S')
@@ -14,6 +15,8 @@ req = requests.get(
     f'{url_loc}?$limit=5000&$where=date_trunc_ymd(data_fi) >= date_trunc_ymd(\'{str_today}\')'
 )
 data = json.loads(req.text)
-with open('data/current_data_loc.json', 'w', encoding='utf-8') as file:
+with open('data/current_data.json', 'w', encoding='utf-8') as file:
     cleaned = DataCleaner.clean_data(data)
-    print(json.dumps(cleaned, indent=4), file=file)
+    enricher = DataEnricher()
+    enriched_data = enricher.enrich_data(cleaned)
+    print(json.dumps(enriched_data, indent=4), file=file)
